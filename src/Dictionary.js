d9aadab746be76d74ef5bc8716a5fc8f73e9ff5a
olenahladkova <hladkova.sk@gmail.com>
2023-01-08 20:19:36 +0100

    api integration

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,10 +1,19 @@
+import axios from "axios";
 import React, { useState } from "react";
 
 export default function Dictionary () {
   let [keyword, setKeyword] = useState("");
+
+  function handleResponse(response) {
+    console.log(response.data)
+  }
+
   function search(event) {
     event.preventDefault();
     alert(`searching for ${keyword} definition`)
+    // documentation https://dictionaryapi.dev/
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
+    axios.get(apiUrl).then(handleResponse)
   }
 
   function handleKeywordChange(event) {
