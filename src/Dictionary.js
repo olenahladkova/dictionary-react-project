46d2885f2c1bbf496c8b43c22a42794b63e91bad
olenahladkova <hladkova.sk@gmail.com>
2023-01-08 21:49:46 +0100

    added new components, definition shows

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,16 +1,19 @@
 import axios from "axios";
 import React, { useState } from "react";
 
+import Results from "./Results";
+
 export default function Dictionary () {
   let [keyword, setKeyword] = useState("");
+  let [results, setResults] = useState(null);
 
   function handleResponse(response) {
-    console.log(response.data)
+    console.log(response.data[0].meanings[0].definitions[0].definition)
+    setResults(response.data[0])
   }
 
   function search(event) {
     event.preventDefault();
-    alert(`searching for ${keyword} definition`)
     // documentation https://dictionaryapi.dev/
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
     axios.get(apiUrl).then(handleResponse)
@@ -26,6 +29,7 @@ export default function Dictionary () {
         <input type="search" className="form-control" onChange={handleKeywordChange}/>
         <input type="submit" value="SEARCH" className="btn button"/>
       </form>
+      <Results results={results} />
     </div>
   )
 }
