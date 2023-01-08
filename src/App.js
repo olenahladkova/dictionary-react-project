2966e9761d05842cdb0926766a6e5562a43e2e3c
olenahladkova <hladkova.sk@gmail.com>
2023-01-08 17:44:57 +0100

    Created react app

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <h1>Definition Dictionary</h1>
+        <hr />
+        <h3>What word are we looking for?</h3>
+      </div>
+    </div>
+  );
+}
+
+export default App;
